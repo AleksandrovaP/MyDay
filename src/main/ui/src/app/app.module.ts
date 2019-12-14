2f4718c7aca2388fae08130dba3a6f56f92f2c94
Petya Aleksandrova <petyaa@vmware.com>
2019-12-14 21:01:52 +0200

    Set for usage clarity ui styles and scripts
    - Set for usage clarity ui styles and scripts

--- src/main/ui/src/app/app.module.ts
+++ src/main/ui/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { AppRouting } from './app.routing';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AccountService } from './service/account.service';
@@ -28,13 +28,17 @@ import { AuthGuard } from './guards/auth-guard';
     AppRouting,
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [
     AccountService,
     AuthGuard,
     AuthenticationService,
     EmployeeService
+  ],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
   ],
   bootstrap: [AppComponent]
 })
